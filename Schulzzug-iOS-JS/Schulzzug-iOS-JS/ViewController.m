d91d6783f0da648f485b9fd9100b24549b237629
Frederik Riedel <frederik.riedel@frogg.io>
2017-03-06 20:23:03 +0100

    added gesture recognizer ios

--- Schulzzug-iOS-JS/Schulzzug-iOS-JS/ViewController.m
+++ Schulzzug-iOS-JS/Schulzzug-iOS-JS/ViewController.m
@@ -22,6 +22,20 @@
     
     [self.webViewGameRenderer loadRequest:[NSURLRequest requestWithURL:url]];
     
+    UISwipeGestureRecognizer *swipeLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+    UISwipeGestureRecognizer *swipeRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipe:)];
+    
+    // Setting the swipe direction.
+    [swipeLeft setDirection:UISwipeGestureRecognizerDirectionLeft];
+    [swipeRight setDirection:UISwipeGestureRecognizerDirectionRight];
+    
+    [self.webViewGameRenderer.scrollView.panGestureRecognizer requireGestureRecognizerToFail:swipeLeft];
+    [self.webViewGameRenderer.scrollView.panGestureRecognizer requireGestureRecognizerToFail:swipeRight];
+    
+    // Adding the swipe gesture on WebView
+    [self.view addGestureRecognizer:swipeLeft];
+    [self.view addGestureRecognizer:swipeRight];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -34,5 +48,23 @@
 -(BOOL)prefersStatusBarHidden {
     return YES;
 }
+
+
+- (void)handleSwipe:(UISwipeGestureRecognizer *)swipe {
+    
+    if (swipe.direction == UISwipeGestureRecognizerDirectionLeft) {
+        NSLog(@"Left Swipe");
+        [self.webViewGameRenderer stringByEvaluatingJavaScriptFromString:@"swipe=1"];
+        
+    }
+    
+    if (swipe.direction == UISwipeGestureRecognizerDirectionRight) {
+        NSLog(@"Right Swipe");
+        [self.webViewGameRenderer stringByEvaluatingJavaScriptFromString:@"swipe=2"];
+        
+    }
+    
+}
+
 
 @end
